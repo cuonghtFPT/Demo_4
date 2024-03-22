065ce896771cc7d01bc6dfa83b2d3859149690b1
cuonghtph34430 <cuonghtph34430@fpt.edu.vn>
2024-03-22 12:18:33 +0700

    Demo

--- App.tsx
+++ App.tsx
@@ -1,118 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [data,setData] =useState<any>();
+  const [img,setImg] = useState<any>();
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{flex:1, alignItems:'center'}}>
+      <Button title='Take photo' onPress={async() => {
+        const result = await launchCamera({saveToPhotos:true,
+        cameraType:'back',mediaType:'photo',quality:1
+        },setData);
+      }}/>
+      <Text style={{fontSize:22}}>{JSON.stringify(data)}</Text>
+
+      <Button title='Take libary' onPress={async() => {
+        const result = await launchImageLibrary({mediaType:'photo',selectionLimit:10
+        },setImg);
+      }}/>
+            <Text style={{fontSize:22}}>{JSON.stringify(img)}</Text>
+      {/* <Text style={{fontSize:22}}>{data.assets[0].originalPath}</Text>
+      <Text style={{fontSize:22}}>{data.assets[0].type}</Text>
+      <Text style={{fontSize:22}}>{data.assets[0].filename}</Text> */}
+
     </View>
-  );
+  )
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default App
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const styles = StyleSheet.create({})
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+// import { Button, StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+// import TrackPlayer from 'react-native-track-player'
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+// const App = () => {
+//   const start = async()=>{
+//     await TrackPlayer.setupPlayer();
 
-export default App;
+//     await TrackPlayer.add({
+//       id:'trackId',
+//       url:require('./gio_lofi.mp3'),
+//       title:'Track Title',
+//       artist:'Track Artist'
+//     });
+//     await TrackPlayer.play();
+//     console.log("dax chay");
+    
+//   }
+//   return (
+//     <View style={{flex:1,alignItems:'center'}}>
+//       <Button title='Player' onPress={start}/>
+//     </View>
+//   )
+// }
+
+// export default App
+
+// const styles = StyleSheet.create({})
---
+++ service.js
@@ -0,0 +1,4 @@
+module.exports = async function() {
+   // Service này cần được đăng ký để module hoạt động
+   // nhưng nó sẽ được sử dụng sau trong phần 'Receiving Events'
+  }
